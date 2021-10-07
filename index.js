b3eda386bb0e41711867c89f485cfb078ae54bd3
VladiSVS <uladzimir.siauko@gmail.com>
2021-10-07 10:46:08 +0200

    fetch

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
 const express = require('express')
+const axios = require('axios');
+require('dotenv').config()
 const app = express()
 //port wo bin ich
 const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-    res.render('index.ejs', { title: 'heroku' })
+    res.render('index.ejs',
+        {
+            title: 'heroku',
+            apiKey: process.env.API_KEY,
+            username: process.env.USERNAME
+        })
 })
 
 app.get('/about', (req, res) => {
     res.render('about.ejs')
+})
+
+app.get('/news', (req, res) => {
+    axios.get(`https://newsapi.org/v2/everything?q=tesla&from=2021-09-07&sortBy=publishedAt&apiKey=${process.env.API_KEY}`)
+        .then(function (response) {
+            // handle success
+            console.log(response.data.articles);
+            res.render('news.ejs', { articles: response.data.articles })
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .then(function () {
+            // always executed
+        });
 })
 
 app.listen(port, () => {
